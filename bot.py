2d14b3ce1b1e202fcce0ffef723869762f00b7a2
Andriі Nikitin <sairusgold@gmail.com>
2025-11-25 15:44:56 +0200

    Fix imports for Render deploy

--- bot.py
+++ bot.py
@@ -1,15 +1,8 @@
-import logging
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    CallbackQueryHandler,
-)
-
-from config import BOT_TOKEN
-
-# handlers
+from telegram.ext import ApplicationBuilder
 from handlers.start import start
+from handlers.menus import menu_callback
 from handlers.digest_fsm import digest_fsm
+
 from handlers.weather import handle_weather_callback
 from handlers.news import handle_news_callback
 from handlers.alerts import handle_alerts_callback
@@ -18,49 +11,31 @@ from handlers.rates import handle_rates_callback
 from handlers.discounts import handle_discounts_callback
 from handlers.afisha import handle_afisha_callback
 
-# menu
-from keyboards.menus import menu_callback
-
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
-)
+from config import BOT_TOKEN
 
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
+    # команда /start
+    app.add_handler(start)
 
     # FSM
     app.add_handler(digest_fsm)
 
-    # Меню
-    app.add_handler(CallbackQueryHandler(menu_callback, pattern="^menu_"))
+    # меню
+    app.add_handler(menu_callback)
 
-    # Погода
-    app.add_handler(CallbackQueryHandler(handle_weather_callback, pattern="^weather_"))
+    # інші обробники
+    app.add_handler(handle_weather_callback)
+    app.add_handler(handle_news_callback)
+    app.add_handler(handle_alerts_callback)
+    app.add_handler(handle_aqi_callback)
+    app.add_handler(handle_rates_callback)
+    app.add_handler(handle_discounts_callback)
+    app.add_handler(handle_afisha_callback)
 
-    # Новини
-    app.add_handler(CallbackQueryHandler(handle_news_callback, pattern="^news_"))
-
-    # Тривоги
-    app.add_handler(CallbackQueryHandler(handle_alerts_callback, pattern="^alerts_"))
-
-    # AQI
-    app.add_handler(CallbackQueryHandler(handle_aqi_callback, pattern="^aqi_"))
-
-    # Курси валют
-    app.add_handler(CallbackQueryHandler(handle_rates_callback, pattern="^rates_"))
-
-    # Знижки
-    app.add_handler(CallbackQueryHandler(handle_discounts_callback, pattern="^discounts_"))
-
-    # Афіша
-    app.add_handler(CallbackQueryHandler(handle_afisha_callback, pattern="^afisha_"))
-
-    print("🔥 Bot is running…")
+    # запуск
     app.run_polling()
 
 
